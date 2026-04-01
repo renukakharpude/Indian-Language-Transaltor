777d787502a79ab4050bc19521e3864ca8d84626
renukakharpude <144589593+renukakharpude@users.noreply.github.com>
2026-04-01 15:58:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-        import streamlit as st
+import streamlit as st
 import base64
 
 # ---------------- BACKGROUND FUNCTION ---------------- #
@@ -356,10 +355,19 @@ elif module == "📄 PDF":
 
         st.success("✅ Translation + Speech Completed!")
 
-   
 
    
+  
+   
+
+
         
+   
+      
+
+       
+
+           
                     
    
 
